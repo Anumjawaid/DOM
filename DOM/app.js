78d8534b762b4c910e90189c47ca4036416f2446
Anumjawaid <anumjawaid99@gmail.com>
2020-07-23 20:32:29 +0500

    DOM Practice

--- DOM/app.js
+++ DOM/app.js
@@ -1,3 +1,10 @@
-var p = document.childNodes[0].childNodes[1].childNodes[1]
-var cn=p.innerHTML
-console.log(cn)
+console.log(document.childNodes[1].childNodes[0].childNodes[5].childNodes[0])//the head part
+console.log(document.childNodes[1].childNodes[2].childNodes[1].childNodes[0])//for body
+// paragraph
+// var p=document.getElementsByTagName('p')
+// var c=p[0].innerHTML
+// console.log(c)
+// excercise
+var c=document.getElementById('main-content')
+for (var i=1;i<10;i+=2)
+{console.log(c.childNodes[i])}
